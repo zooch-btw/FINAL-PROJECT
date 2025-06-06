91f22a7e6280a36184adeb59fca459f946e0cdc5
zooch-btw <526SDiPalma@frhsd.com>
2025-06-06 09:28:02 -0400

    Finished landing site (maybe?)

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const greetings = [
-    { start: 5, end: 11, text: "Good morning!", cls: "morning", glow: "VictoryTxt" },
-    { start: 11, end: 17, text: "Good afternoon!", cls: "afternoon", glow: "VictoryTxt" },
-    { start: 17, end: 22, text: "Good evening!", cls: "evening", glow: "LossTxt" },
-    { start: 22, end: 24, text: "Good night!", cls: "night", glow: "LossTxt" },
-    { start: 0, end: 5, text: "Good night!", cls: "night", glow: "LossTxt" }
-];
-
-window.addEventListener("DOMContentLoaded", () => {
-    const hr = new Date().getHours();
-    const g = greetings.find(({ start, end }) => hr >= start && hr < end);
-    document.body.classList.add(g.cls);
-    const greetingEl = document.getElementById("greeting");
-    greetingEl.textContent = g.text;
-    greetingEl.id = g.glow;
-    setTimeout(() => window.location.replace("about.html"), 5000);
-});
+const DOC = {
+    e: document,
+    get(arg) {
+        return this.e.querySelector(arg);
+    },
+    getALL(arg) {
+        return Array.from(this.e.querySelectorAll(arg));
+    }
+}
+const PAGE = {
+    greeting() {
+        const greetings = [
+            { start: 5, end: 11, text: "Good morning!", cls: "morning", glow: "VictoryTxt" },
+            { start: 11, end: 17, text: "Good afternoon!", cls: "afternoon", glow: "VictoryTxt" },
+            { start: 17, end: 22, text: "Good evening!", cls: "evening", glow: "LossTxt" },
+            { start: 22, end: 24, text: "Good night!", cls: "night", glow: "LossTxt" },
+            { start: 0, end: 5, text: "Good night!", cls: "night", glow: "LossTxt" }
+        ];
+        const hr = new Date().getHours();
+        const g = greetings.find(({ start, end }) => hr >= start && hr < end);
+        document.body.classList.add(g.cls);
+        const greetingEl = document.getElementById("greeting");
+        greetingEl.textContent = g.text;
+        greetingEl.id = g.glow;
+    }
+}
+window.addEventListener("DOMContentLoaded", PAGE.greeting);

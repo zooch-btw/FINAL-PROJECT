3ea93f19e005fa0453a913a41781408b6d37c251
zooch-btw <526SDiPalma@frhsd.com>
2025-06-18 09:18:09 -0400

    contact page works pt2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 const navbar = `<nav class="navbar navbar-expand-lg" id="main-nav">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.html"><img src="imgs/Little Bird Toys Logo (1).png" class="birdie"
+                <a class="navbar-brand" href="index.html"><img src="imgs/Little Bird Toys Logo (1).png" class="birdie" style="border-radius: 100%;"
                         alt="al1"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
